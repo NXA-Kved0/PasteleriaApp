9fd5473af674860f58729d246fc166475e224757
Ignacia <ig.cavedo@duocuc.cl>
2025-10-28 09:38:01 -0300

    SQLite

--- app/src/main/java/com/example/sqlite/MainActivity.kt
+++ app/src/main/java/com/example/sqlite/MainActivity.kt
@@ -1,37 +1,53 @@
 package com.example.sqlite
-import UserScreen
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import com.example.sqlite.ui.theme.SQLITETheme
-import androidx.room.Room
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.room.Room
 import com.example.sqlite.data.local.AppDatabase
 import com.example.sqlite.data.repository.UserRepository
+import com.example.sqlite.ui.users.UserScreen
 import com.example.sqlite.viewmodel.UserViewModel
 import com.example.sqlite.viewmodel.UserViewModelFactory
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         val db = Room.databaseBuilder(
             applicationContext,
             AppDatabase::class.java,
             "my_database"
         ).build()
+
         val repo = UserRepository(db.userDao())
         val factory = UserViewModelFactory(repo)
+
         setContent {
-            val viewModel: UserViewModel = viewModel(factory = factory)
-            SQLITETheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    UserScreen(viewModel,
-                        Modifier.fillMaxSize().padding(innerPadding))
-                }
+            MaterialTheme {
+                MainContent(factory)
             }
         }
+    }
+}
+
+@Composable
+fun MainContent(factory: UserViewModelFactory) {
+    val viewModel: UserViewModel = viewModel(factory = factory)
+
+    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+        UserScreen(
+            viewModel = viewModel,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+        )
     }
 }
